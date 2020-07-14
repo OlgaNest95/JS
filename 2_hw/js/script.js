7d7f6a0a4cc759941f6158738242c906182e9bf1
OlgaNest95 <61617301+OlgaNest95@users.noreply.github.com>
2020-07-14 22:53:52 +0300

    Add files via upload

---
+++ 2_hw/js/script.js
@@ -0,0 +1,34 @@
+let firstRangeNumber = +prompt ('Please, fill in the first number of range ');
+let lastRangeNumber = +prompt ('Please, fill in the last number of range');
+for (i=firstRangeNumber; i <= lastRangeNumber; i++) {
+    if (i%5 ===0 || i%2 ===0) {
+        console.log (i);
+    }
+};
+for (i=firstRangeNumber; i >= lastRangeNumber; i--) {
+    if (i%5 ===0 || i%2 ===0) {
+        console.log (i);
+    }
+};
+
+
+
+let answerName = prompt ('Привет! Как тебя зовут?');
+let answerAffairs = prompt ('Как ' + name+ ' Ваши дела?');
+let answerFood = prompt ('Чем потчевал сегодня барин?');
+let answerGoodbye = prompt ('К сожалению я робот и не кушаю, поэтому ваши человечьи вкусы мне не близки. Прощайте....');
+console.log('a. - Привет! Как тебя зовут?');
+console.log('b. - '+ answerName);
+console.log('c. - Как ' + name+ ' Ваши дела?');
+console.log('d. - '+ answerAffairs);
+console.log('e. - Чем потчевал сегодня барин?');
+console.log('f. - '+ answerFood);
+console.log('g. - К сожалению я робот и не кушаю, поэтому ваши человечьи вкусы мне не близки. Прощайте....');
+console.log('h. - '+ answerGoodbye);
+
+
+
+
+
+
+
