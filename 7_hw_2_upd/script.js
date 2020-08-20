c4cb1adb200cd1bddc11470db7a3f9225be70fb8
OlgaNest95 <61617301+OlgaNest95@users.noreply.github.com>
2020-08-20 10:24:07 +0300

    Add files via upload

---
+++ 7_hw_2_upd/script.js
@@ -0,0 +1,5 @@
+//2
+let divByClassWords = document.body.querySelector('div');
+console.log(divByClassWords)
+let divByCssSelector  = document.querySelectorAll('.number');
+console.log(divByCssSelector)
